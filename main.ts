791dd25813ce7b41ab9ddf05101570d6d8b5ad14
CBIS2022oscar <123550530+CBIS2022oscar@users.noreply.github.com>
2023-03-22 11:57:37 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onGesture(Gesture.Shake, function () {
+    Scared = images.createImage(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+let Scared: Image = null
+let Laugh = images.createImage(`
+    . # . # .
+    # . . . #
+    . . . . .
+    # # # # #
+    . # # # .
+    `)
+let Sad = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+let Happy = images.createImage(`
+    . # . # .
+    . . . . .
+    # # # # #
+    # . . . #
+    . # # # .
+    `)
+basic.forever(function () {
+	
+})
