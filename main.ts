e0e966b3a98dd94563db26b24b491385567e4b82
CBIS2022oscar <123550530+CBIS2022oscar@users.noreply.github.com>
2023-04-12 10:56:57 +0100

    Much more features

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ input.onButtonPressed(Button.A, function () {
         # . . . #
         . # # # .
         `)
-    music.playMelody("C5 A B A C5 B A C5 ", 170)
+    music.playMelody("B A E - F - G C5 ", 120)
 })
 input.onGesture(Gesture.LogoUp, function () {
     Laugh = images.createImage(`
@@ -16,7 +16,20 @@ input.onGesture(Gesture.LogoUp, function () {
         # . . . #
         . # # # .
         `)
-    music.playMelody("B A E - F - G C5 ", 120)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 5000, 1984, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    music.playMelody("G B A G C5 B A B ", 120)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    Angry = images.createImage(`
+        # . . . #
+        # # . # #
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.soaring), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 3558, 2085, 255, 255, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    music.setVolume(255)
 })
 input.onSound(DetectedSound.Loud, function () {
     Sad = images.createImage(`
@@ -36,7 +49,7 @@ input.onButtonPressed(Button.AB, function () {
         # . # . #
         . # . # .
         `)
-    music.playMelody("C5 B C5 A B C5 B C5 ", 120)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 5000, 2385, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
 })
 input.onGesture(Gesture.Shake, function () {
     Scared = images.createImage(`
@@ -51,13 +64,19 @@ input.onGesture(Gesture.Shake, function () {
 let Scared: Image = null
 let Pat: Image = null
 let Sad: Image = null
+let Angry: Image = null
 let Laugh: Image = null
 let fun: Image = null
 let Idle = images.createImage(`
     . # . # .
     . . . . .
     . . . . .
-    . # # # .
+    # # # # #
     . . . . .
     `)
 music.playMelody("G E G - G - G E ", 120)
+basic.forever(function () {
+    if (input.temperature() == 20) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.InBackground)
+    }
+})
